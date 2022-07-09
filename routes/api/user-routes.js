6682f8a6c443511ee44261cc4b146d126d91f075
DukeHdez <briayan27@outlook.com>
2022-07-09 15:23:35 -0400

    Fix thoughts routes, and added friends routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -4,21 +4,27 @@ const {
     getAllUsers,
     getUserById,
     createUser,
+    addFriend,
     updateUser,
-    deleteUser
+    deleteUser,
+    deleteFriend
 } = require('../../controllers/user-controller')
 
 // GET & POST set up @ /api/users
 router
-.route('/')
-.get(getAllUsers)
-.post(createUser);
+    .route('/')
+    .get(getAllUsers)
+    .post(createUser);
 
 // GET by Id, PUT & DELETE set up @ /api/users/:id
 router
-.route('/:id')
-.get(getUserById)
-.put(updateUser)
-.delete(deleteUser)
+    .route('/:id')
+    .get(getUserById)
+    .put(updateUser)
+    .delete(deleteUser)
 
+router
+    .route('/:userId/friends/:friendId')
+    .put(addFriend)
+    .delete(deleteFriend)
 module.exports = router;
