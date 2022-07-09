d9f95c6a13993d6ac653b47bcaa70f6158f8c5aa
DukeHdez <briayan27@outlook.com>
2022-07-09 12:00:42 -0400

    Add user model

---
+++ models/Reaction.js
@@ -0,0 +1,25 @@
+const { Schema, model, Types } = require('mongoose');
+
+const ReactionSchema = new Schema(
+    {
+        reactionId: {
+            type: Schema.Types.ObjectId,
+            default: new ObjectId
+        },
+        reactionBody: {
+            type: String,
+            required: true,
+            max: 280
+        },
+        username: {
+            type: String,
+            required: true
+        },
+        createdAt: {
+            Type: Date,
+            default: Date.now
+        }
+    }
+);
+
+module.exports = ReactionSchema;
